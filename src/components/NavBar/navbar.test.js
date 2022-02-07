7c2574428b4d63a5e98f33126af3837ec38988f7
Suhas Sanmukh <sbs.max2c@gmail.com>
2022-02-07 23:51:58 +0100

    test: add first successful unit test

--- src/components/NavBar/navbar.test.js
+++ src/components/NavBar/navbar.test.js
@@ -1,31 +1,26 @@
-/**
- * @jest-environment jsdom
- */
-
-import { mount } from '@vue/test-utils';
 import NavBar from './NavBar.vue';
+import { shallowMount } from '@vue/test-utils';
 
-
-// The component to test
-const TestComponent = {
-  template: '<p>{{ msg }}</p>',
-  props: ['msg']
-}
-
-/* This test works */
-test('displays message', () => {
-  const wrapper = mount(TestComponent, {
-    props: {
-      msg: 'Hello world'
-    }
+describe('NavBar.vue', () => {
+  it('render the component', () => {
+    const wrapper = shallowMount(NavBar, {
+      props: {}
+    });
+    expect(wrapper.text()).toMatch('')
   })
-  expect(wrapper.text()).toContain('Hello world')
+  it('render with home button', () => {
+    const wrapper = shallowMount(NavBar, {
+      props: { showHomebutton: true }
+    });
+    const homeBtn = wrapper.get('.nav-label');
+    expect(homeBtn.text()).toMatch('Home')
+  })
+  it('render with slot content', () => {
+    const wrapper = shallowMount(NavBar, {
+      slots: {
+        default: 'Simple text'
+      }
+    });
+    expect(wrapper.text()).toMatch('Simple text')
+  })
 })
-
-// TODO : The Webpack setup for Vue 3 + Jest is not working! Facing Error on import { mount } from '@vue/test-utils'; : SyntaxError: Cannot use import statement outside a module
-// I have tried multiple approaches to fix it via babel, somehow it's not suporting ES6 modules for unit test.
-
-test('Loads Navbar', () => {
-  const wrapper = mount(NavBar)
-  expect(wrapper.text()).toContain('Home')
-});
