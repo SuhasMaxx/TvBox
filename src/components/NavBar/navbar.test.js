f7cb0b923e8fc2734bb642de1418087fc8445f69
Suhas Sanmukh <sbs.max2c@gmail.com>
2022-02-07 15:39:05 +0100

    docs: update unit test issue

--- src/components/NavBar/navbar.test.js
+++ src/components/NavBar/navbar.test.js
@@ -12,6 +12,7 @@ const TestComponent = {
   props: ['msg']
 }
 
+/* This test works */
 test('displays message', () => {
   const wrapper = mount(TestComponent, {
     props: {
@@ -20,6 +21,9 @@ test('displays message', () => {
   })
   expect(wrapper.text()).toContain('Hello world')
 })
+
+// TODO : The Webpack setup for Vue 3 + Jest is not working! Facing Error on import { mount } from '@vue/test-utils'; : SyntaxError: Cannot use import statement outside a module
+// I have tried multiple approaches to fix it via babel, somehow it's not suporting ES6 modules for unit test.
 
 test('Loads Navbar', () => {
   const wrapper = mount(NavBar)
